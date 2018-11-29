f4c18f74b6b289f4efc7bdbdca10aca59902e308
Somesh Kar <14039437+someshkar@users.noreply.github.com>
2018-11-29 15:41:26 +0530

    Updates

---
+++ precache-manifest.881be2f0da3248e266e4cee99f9420a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "92d7e62153d1416c7923",
+    "url": "/static/js/main.92d7e621.chunk.js"
+  },
+  {
+    "revision": "f337491d15e60188c8f6",
+    "url": "/static/js/1.f337491d.chunk.js"
+  },
+  {
+    "revision": "92d7e62153d1416c7923",
+    "url": "/static/css/main.533568ba.chunk.css"
+  },
+  {
+    "revision": "00d2afd9e8cae72310ce5ba3fcae4d89",
+    "url": "/index.html"
+  }
+];
